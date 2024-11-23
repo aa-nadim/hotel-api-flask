1b1566674502bf32a803893872cc1aff35d25663
aa-nadim <nadim.ice.nstu@gmail.com>
2024-11-23 18:30:27 +0600

    remove config.py file

--- user_service/config.py
+++
@@ -1,8 +0,0 @@
-# user_service/config.py
-class Config:
-    JWT_SECRET_KEY = 'your-secret-key'
-    SWAGGER = {
-        "title": "User Service API",
-        "description": "API for user registration, authentication, and profile management",
-        "version": "1.0.0"
-    }
